4e96707acde88dfc9500951f4035faeaf885a471
DouglasRfV <douglas.rfvianna@gmail.com>
2020-10-20 23:10:46 -0300

    start projeto

---
+++ core/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import (
+    Marca, 
+    Veiculo, 
+    Pessoa, 
+    Parametros, 
+    MovRotativo,
+    Mensalista,
+    MovMensalista
+)
+
+class MovRotativoAdmin(admin.ModelAdmin):
+    list_display = (
+        'veiculo', 'checkin', 'checkout', 'valor_hora', 'veiculo', 'horas_total', 'total', 'pago' 
+    )
+
+
+class MovMensalistaAdmin(admin.ModelAdmin):
+    list_display = (
+        'mensalista', 'dt_pgto', 'total'
+    )
+
+    def veiculo(self, obj):
+        return obj.veiculo
+
+admin.site.register(Marca)
+admin.site.register(Veiculo)
+admin.site.register(Pessoa)
+admin.site.register(Parametros)
+admin.site.register(MovRotativo, MovRotativoAdmin)
+admin.site.register(Mensalista)
+admin.site.register(MovMensalista, MovMensalistaAdmin)
