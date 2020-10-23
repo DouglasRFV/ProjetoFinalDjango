a4d061f5296d2a1ac1f6d4ed0e22ae17314d8ac9
DouglasRfV <douglas.rfvianna@gmail.com>
2020-10-23 00:20:40 -0300

    finalizando cruds

--- core/urls.py
+++ core/urls.py
@@ -2,31 +2,56 @@ from django.urls import include, re_path
 from .views import (
     home, 
     lista_pessoas, 
-    lista_veiculos, 
-    lista_movRotativos,
-    lista_mensalistas,
-    lista_movMensalistas,
     pessoa_novo,
+    pessoa_update,
+    pessoa_delete,
+
+    lista_veiculos, 
     veiculo_novo,
+    veiculo_update,
+    veiculo_delete,
+    
+    lista_movRotativos,
     movRotativo_novo,
+    movRotativo_update,
+    movRotativo_delete,
+    
+    lista_mensalistas,
     mensalista_novo,
-    movMensalistas_novo
+    mensalista_update,
+    mensalista_delete,
+    
+    lista_movMensalistas,
+    movMensalistas_novo,
+    movMensalista_update,
+    movMensalista_delete,
 )    
 
 urlpatterns = [
     re_path(r'^$', home, name='core_home'),
     re_path(r'^pessoas/$', lista_pessoas, name='core_lista_pessoas'),
     re_path(r'^pessoa-novo/$', pessoa_novo, name='core_pessoa_novo'),
+    re_path(r'^pessoa-update/(?P<id>\d+)/$', pessoa_update, name='core_pessoa_update'),
+    re_path(r'^pessoa-delete/(?P<id>\d+)/$', pessoa_delete, name='core_pessoa_delete'),
 
     re_path(r'^veiculos/$', lista_veiculos, name='core_lista_veiculos'),
     re_path(r'^veiculo-novo/$', veiculo_novo, name='core_veiculo_novo'),
+    re_path(r'^veiculo-update/(?P<id>\d+)/$', veiculo_update, name='core_veiculo_update'),
+    re_path(r'^veiculo-delete/(?P<id>\d+)/$', veiculo_delete, name='core_veiculo_delete'),
 
     re_path(r'^movRotativos/$', lista_movRotativos, name='core_lista_movRotativos'),
     re_path(r'^movRotativo-novo/$', movRotativo_novo, name='core_movRotativo_novo'),
+    re_path(r'^movRotativo-update/(?P<id>\d+)/$', movRotativo_update, name='core_movRotativo_update'),
+    re_path(r'^movRotativo-delete/(?P<id>\d+)/$', movRotativo_delete, name='core_movRotativo_delete'),
 
-    re_path(r'^mensalista/$', lista_mensalistas, name='core_lista_mensalista'),
+    re_path(r'^mensalista/$', lista_mensalistas, name='core_lista_mensalistas'),
     re_path(r'^mensalista-novo/$', mensalista_novo, name='core_mensalista_novo'),
+    re_path(r'^mensalista-update/(?P<id>\d+)/$', mensalista_update, name='core_mensalista_update'),
+    re_path(r'^mensalista-delete/(?P<id>\d+)/$', mensalista_delete, name='core_mensalista_delete'),
 
     re_path(r'^movMensalistas/$', lista_movMensalistas, name='core_lista_movMensalistas'),
     re_path(r'^movMensalista-novo/$', movMensalistas_novo, name='core_movMensalista_novo'),
+    re_path(r'^movMensalista-update/(?P<id>\d+)/$', movMensalista_update, name='core_movMensalista_update'),
+    re_path(r'^movMensalista-delete/(?P<id>\d+)/$', movMensalista_delete, name='core_movMensalista_delete'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -18,6 +18,7 @@ def home(request):
     context = {'mensagem': 'Olá Mundo!'}
     return render(request, 'core/index.html', context)
 
+#===========================PESSOAS==================================
 
 def lista_pessoas(request):
     pessoas = Pessoa.objects.all()
@@ -31,6 +32,33 @@ def pessoa_novo(request):
     if form.is_valid():
         form.save()
     return redirect('core_lista_pessoas')
+
+
+def pessoa_update(request, id):
+    data = {}
+    pessoa = Pessoa.objects.get(id=id)
+    form = PessoaForm(request.POST or None, instance=pessoa)
+    data['pessoa'] = pessoa
+    data['form'] = form
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('core_lista_pessoas')
+    else:
+        return render(request, 'core/update_pessoa.html', data)
+
+
+def pessoa_delete(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+    if request.method == 'POST':
+        pessoa.delete()
+        return redirect('core_lista_pessoas')
+    else:
+        return render(request, 'core/delete_confirm.html', {'objDelete': pessoa})
+
+
+#===========================VEÍCULOS==================================
 
 
 def lista_veiculos(request):
@@ -47,6 +75,33 @@ def veiculo_novo(request):
     return redirect('core_lista_veiculos')
 
 
+def veiculo_update(request, id):
+    data = {}
+    veiculo = Veiculo.objects.get(id=id)
+    form = VeiculoForm(request.POST or None, instance=veiculo)
+    data['veiculo'] = veiculo
+    data['form'] = form
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('core_lista_veiculos')
+    else:
+        return render(request, 'core/update_veiculo.html', data)
+
+
+def veiculo_delete(request, id):
+    veiculo = Veiculo.objects.get(id=id)
+    if request.method == 'POST':
+        veiculo.delete()
+        return redirect('core_lista_veiculos')
+    else:
+        return render(request, 'core/delete_confirm.html', {'objDelete': veiculo})
+
+
+#===========================MOVIMENTO ROTATIVOS==================================
+
+
 def lista_movRotativos(request):
     movRotativos = MovRotativo.objects.all()
     form = MovRotativoForm()
@@ -61,6 +116,33 @@ def movRotativo_novo(request):
     return redirect('core_lista_movRotativos')
 
 
+def movRotativo_update(request, id):
+    data = {}
+    movRotativo = MovRotativo.objects.get(id=id)
+    form = MovRotativoForm(request.POST or None, instance=movRotativo)
+    data['movRotativo'] = movRotativo
+    data['form'] = form
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('core_lista_movRotativos')
+    else:
+        return render(request, 'core/update_movRotativo.html', data)
+
+
+def movRotativo_delete(request, id):
+    movRotativo = MovRotativo.objects.get(id=id)
+    if request.method == 'POST':
+        movRotativo.delete()
+        return redirect('core_lista_movRotativos')
+    else:
+        return render(request, 'core/delete_confirm.html', {'objDelete': movRotativo})
+
+
+#===========================MENSALISTAS==================================
+
+
 def lista_mensalistas(request):
     mensalistas = Mensalista.objects.all()
     form = MensalistaForm()
@@ -72,7 +154,34 @@ def mensalista_novo(request):
     form = MensalistaForm(request.POST or None)
     if form.is_valid():
         form.save()
-    return redirect('core_lista_mensalista')
+    return redirect('core_lista_mensalistas')
+
+
+def mensalista_update(request, id):
+    data = {}
+    mensalista = Mensalista.objects.get(id=id)
+    form = MensalistaForm(request.POST or None, instance=mensalista)
+    data['mensalista'] = mensalista
+    data['form'] = form
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('core_lista_mensalistas')
+    else:
+        return render(request, 'core/update_mensalista.html', data)
+
+
+def mensalista_delete(request, id):
+    mensalista = Mensalista.objects.get(id=id)
+    if request.method == 'POST':
+        mensalista.delete()
+        return redirect('core_lista_mensalistas')
+    else:
+        return render(request, 'core/delete_confirm.html', {'objDelete': mensalista})
+
+
+#===========================MOVIMENTO MENSALISTAS==================================
 
 
 def lista_movMensalistas(request):
@@ -87,3 +196,27 @@ def movMensalistas_novo(request):
     if form.is_valid():
         form.save()
     return redirect('core_lista_movMensalistas')
+
+
+def movMensalista_update(request, id):
+    data = {}
+    movMensalista = MovMensalista.objects.get(id=id)
+    form = MovMensalistaForm(request.POST or None, instance=movMensalista)
+    data['movMensalista'] = movMensalista
+    data['form'] = form
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('core_lista_movMensalistas')
+    else:
+        return render(request, 'core/update_movMensalista.html', data)
+
+
+def movMensalista_delete(request, id):
+    movMensalista = MovMensalista.objects.get(id=id)
+    if request.method == 'POST':
+        movMensalista.delete()
+        return redirect('core_lista_movMensalistas')
+    else:
+        return render(request, 'core/delete_confirm.html', {'objDelete': movMensalista})
