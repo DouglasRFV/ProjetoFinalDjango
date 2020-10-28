31d44ad47808ddd22d95724974ebc2b7715296d8
DouglasRfV <douglas.rfvianna@gmail.com>
2020-10-28 20:35:31 -0300

    finalizando template e submit form

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,17 @@
 from django.urls import include, re_path, path
 from .views import (
     home, 
+    contato,
+    servicos,
+    sobre,
+    planos
 )    
 
 urlpatterns = [
     re_path(r'^$', home, name='website_home'),
+    re_path(r'^contato$', contato, name='website_contato'),
+    re_path(r'^servicos$', servicos, name='website_servicos'),
+    re_path(r'^sobre$', sobre, name='website_sobre'),
+    re_path(r'^planos$', planos, name='website_planos'),
 
 ]
